ce6e0406d049eaba7b3c67ed105b08fa714b1069
Barld Boot <bakeds@gmail.com>
2014-06-09 14:59:37 +0200

    geeft juiste positie door aan het boord alleen de start positie moet bepaald worden en met hoeveel het vermenigvuldigd moet worden om een hokje te krijgen

--- GUIWinForm/BordPositions.cs
+++ GUIWinForm/BordPositions.cs
@@ -10,18 +10,79 @@ namespace GUIWinForm
     {
         private List<posafwijking> lafwijkingen = new List<posafwijking>();
 
-
-        public void SetAfwijkingen()
+        public BordPositions()
         {
+            this.SetAfwijkingen();
+        }
+
+        /// <summary>
+        /// word een keer door de constructor aangeroepen
+        /// </summary>
+        void SetAfwijkingen()
+        {
+            #region omslagpunten
+            //defineer lijst met 4 spelers bord
             lafwijkingen = new List<posafwijking>
             {
                 new posafwijking("y", -4),
                 new posafwijking("x", 4),
                 new posafwijking("y", -2),
                 new posafwijking("x", -4),
-
+                new posafwijking("y", -4),
+                new posafwijking("x", -2),
+                new posafwijking("y", 4),
+                new posafwijking("x", -4),
+                new posafwijking("y", 2),
+                new posafwijking("x", 4),
+                new posafwijking("y", 4),
+                new posafwijking("x", 1)
             };
+            #endregion
         }
+
+        public System.Drawing.Point GetPosition(int pos)
+        {
+            int x = 0;
+            int y = 0;
+            //hokje in speelveld
+            if(pos<40)
+            {
+                int item = 0;                
+                while(pos>0)
+                {
+                    if(Math.Abs( this.lafwijkingen[item].afwijking) > pos)
+                    {
+                        if(this.lafwijkingen[item].vorm == "x")
+                        {
+                            x += pos;
+                        }
+                        else
+                        {
+                            y += pos;
+                        }
+                        pos = 0;
+                        break;
+                    }
+                    else
+                    {
+                        if (this.lafwijkingen[item].vorm == "x")
+                        {
+                            x += this.lafwijkingen[item].afwijking;
+                        }
+                        else
+                        {
+                            y += this.lafwijkingen[item].afwijking;
+                        }
+                        pos -= (int)Math.Abs(this.lafwijkingen[item].afwijking);
+                    }
+                    item++;
+                }
+            }
+
+            return new System.Drawing.Point(x, y);
+        }
+
+
     }
 
     class posafwijking
